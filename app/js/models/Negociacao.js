cd4dd05f8fa2e1e4fc929e4b86d593c52fc4dc1e
luccasadriano <luccas-adriano@hotmail.com>
2021-01-06 23:33:00 -0300

    01 Bem começado, metade feito

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,19 @@
+class Negociacao {
+   constructor(data, quantidade, valor) {
+      this._data = data
+      this._quantidade = quantidade
+      this._valor = valor
+   }
+   get data(){
+      return this._data
+   }
+   get quantidade(){
+      return this.quantidade
+   }
+   get valor(){
+      return this.valor
+   }
+   get volume(){
+      return this.quantidade * this.valor
+   }
+}
