b8686d363e81db87a7fd4c3f84e65ddc68e67e54
luccasadriano <luccas-adriano@hotmail.com>
2021-01-12 21:08:43 -0300

    04 Herança, reaproveitamento de código

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -5,6 +5,7 @@ class NegociacaoController {
    private _inputValor: HTMLInputElement
    private _negociacoes = new Negociacoes()
    private _negociacoesView = new NegociacoesView('#negociacoesView')
+   private _mensagemView = new MensagemView('#mensagemView')
 
    constructor() {
       this._inputData = <HTMLInputElement>document.querySelector('#data')
@@ -23,8 +24,12 @@ class NegociacaoController {
         parseFloat(this._inputValor.value)
       )
       this._negociacoes.adicionar(negociacao)
+
       this._negociacoesView.update(this._negociacoes)
 
+      this._mensagemView.update("Negociação adicionada com sucesso!")
+
+      
 
    }
 
